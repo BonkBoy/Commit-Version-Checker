76f7931546d98dbdc69bb4ddb01398ba5c52bc2b
Brendan Kedwards <bkedwards03@gmail.com>
2024-12-10 13:16:33 +0000

    Added action

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM alpine:latest
+
+RUN apk --update --no-cache add git
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT [ "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -ue
+
+version_lte() {
+  printf '%s\n' "$1" "$2" | sort -C -V
+}
+
+PathToVersion="${INPUT_VERSION_PATH}"
+CurrentVersion="$(jq -r .version /${PathToVersion})"
+
+git checkout main
+
+MainVersion="$(jq -r .version /${PathToVersion})"
+
+version_lte "${MainVersion}" "${PathToVersion}"
